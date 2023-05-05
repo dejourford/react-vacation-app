547d9af1bf30dc294bd9ca283be570ba90ef965c
dejourford <dejourford@my.unt.edu>
2023-05-05 10:15:24 -0500

    add text to header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./header.css"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <i className="fa-solid fa-globe"></i>
+            <p>my travel journal.</p>
+        </div>
+    )
+}
